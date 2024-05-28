5be6f2a1e3a2507bcac04498ca5f13cff137f980
carsonkimck <carsonkim029@gmail.com>
2024-05-28 12:20:34 -0700

    edited username

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ def generate_report(result_rates, avg_moves, common_opening, username):
 
 
 def main(): 
-    username = "Jacobyounessi"
+    username = "erik"
 
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
